532a53138a474d5bb2bb208628f1949ed04adf3e
smmohiuddin-enosisbd <abdur.tumzied@enosisbd.com>
2022-05-09 12:19:33 +0600

    nc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   title = 'Template driven forms';
+  contact!:Contact;
+
+  ngOnInit(): void {
+    this.contact = {
+      firstname: "Sachin",
+      lastname: "Tendulkar",
+      email: "sachin@gmail.com",
+      gender: "male",
+      isMarried: true,
+      country: "2",
+      address: { city: "Mumbai", street: "Perry Cross Rd", pincode: "400050" }
+    };
+  }
+
  
   countryList:country[] = [
     new country("1", "India"),
@@ -15,8 +30,11 @@ export class AppComponent {
     new country('3', 'England')
   ];
 
-  onSubmit(contactForm:any) {
+  onSubmit(contactForm:NgForm) {
     console.log(contactForm.value);
+  }
+  resetForm(contactForm:NgForm){
+    contactForm.resetForm();
   }
 }
 
@@ -31,6 +49,18 @@ export class country {
   }
 }
 
- 
+interface Contact {
+  firstname:string;
+  lastname:string;
+  email:string;
+  gender:string;
+  isMarried:boolean;
+  country:string;
+  address: {
+    city:string;
+    street:string;
+    pincode:string;
+  }
+} 
 
  
