bc897cfd963a367df83dd936045c222df0805018
smmohiuddin-enosisbd <abdur.tumzied@enosisbd.com>
2022-05-09 14:52:51 +0600

    nc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-
+export class AppComponent implements OnInit {
   title = 'Template driven forms';
- 
-  countryList:country[] = [
-    new country("1", "India"),
+  contactForm = new FormGroup({
+    firstname: new FormControl({value:"raj",disabled:false  },[Validators.required,Validators.min(3)]),
+    lastname: new FormControl(),
+    email: new FormControl(),
+    gender: new FormControl(),
+    isMarried: new FormControl(),
+    country: new FormControl(),
+    address: new FormGroup({
+      city: new FormControl(),
+      street: new FormControl(),
+      pincode: new FormControl(),
+    }),
+  });
+
+  countryList: country[] = [
+    new country('1', 'India'),
     new country('2', 'USA'),
-    new country('3', 'England')
+    new country('3', 'England'),
   ];
 
-  onSubmit(contactForm:any) {
-    console.log(contactForm.value);
+  ngOnInit(): void {
+    this.contactForm.get('firstname')?.statusChanges.subscribe(status=>{
+      console.log(status)
+      console.log(this.contactForm.dirty);
+    });
+  }
+
+  onSubmit() {
+    console.log(this.contactForm.value);
+    console.log(this.contactForm.dirty);
   }
 }
-
 
 export class country {
-  id:string;
-  name:string;
- 
-  constructor(id:string, name:string) {
-    this.id=id;
-    this.name=name;
+  id: string;
+  name: string;
+
+  constructor(id: string, name: string) {
+    this.id = id;
+    this.name = name;
   }
 }
-
- 
-
- 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';        //import FormsModule
+import { ReactiveFormsModule } from '@angular/forms';        //import FormsModule
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule                    //Add in Imports Array
+    ReactiveFormsModule                    //Add in Imports Array
   ],
   providers: [],
   bootstrap: [AppComponent]
