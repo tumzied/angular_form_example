2138ba430745dc96cee8b8b0938494d032d3d179
smmohiuddin-enosisbd <abdur.tumzied@enosisbd.com>
2022-05-09 11:48:26 +0600

    basic -template-driven form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'form_example';
+
+  title = 'Template driven forms';
+ 
+  countryList:country[] = [
+    new country("1", "India"),
+    new country('2', 'USA'),
+    new country('3', 'England')
+  ];
+
+  onSubmit(contactForm:any) {
+    console.log(contactForm.value);
+  }
 }
+
+
+export class country {
+  id:string;
+  name:string;
+ 
+  constructor(id:string, name:string) {
+    this.id=id;
+    this.name=name;
+  }
+}
+
+ 
+
+ 
